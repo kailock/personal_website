63702dbf8424d019a1b7a4f4ca5b54e49d77ee83
kailock <kailock@uw.edu>
2016-01-20 23:04:33 -0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Kaitlin on 1/10/2016.
+ */
+
+var myApp = angular.module('myApp', ['ui.router']);
+
+myApp.config(function($stateProvider) {
+    $stateProvider.state('about', {
+        url:'/',
+        templateUrl: 'templates/about.html',
+        controller: 'AboutController'
+    })
+        .state('portfolio', {
+        url:'/portfolio',
+        templateUrl: 'templates/portfolio.html',
+        controller: "PortfolioController"
+    })
+        .state('resume', {
+            url:'/resume',
+            templateUrl: 'templates/resume.html',
+            controller: 'ResumeController'
+        })
+        .state('contact', {
+            url:'/contact',
+            templateUrl: 'templates/contact.html',
+            controller: 'ContactController'
+        })
+})
+    .controller('AboutController', function($scope) {
+
+    })
+    .controller('PortfolioController', function($scope, $http) {
+
+    })
+    .controller('ResumeController', function($scope) {
+
+    })
+    .controller('ContactController', function($scope) {
+
+    });
+
+//$(function () {
+//    $('[data-toggle="popover"]').popover()
+//    $('[data-toggle="tooltip"]').tooltip()
+//});
+
+$(function(){
+    $('body').popover({ selector: '[data-toggle="popover"]' });
+    $('a').popover({ selector: '[data-trigger="focus"]' });
+});
+
+//$('[rel=tooltip]').tooltip();
+//$(document).on('focus', '[rel=tooltip]', function () { $(this).tooltip('show'); });
